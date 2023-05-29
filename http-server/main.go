d35a8e29565f71862efa7aeddb7f0934e881e787
sheng <chengzhisheng2001@gmail.com>
2023-05-29 22:18:28 +0800

    Add Hertz functionality

--- http-server/main.go
+++ http-server/main.go
@@ -5,12 +5,29 @@ import (
 	"log"
 	"time"
 
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/app/server"
+    "github.com/cloudwego/hertz/pkg/common/utils"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+
+	"github.com/cloudwego/kitex/client"
 	"github.com/tim-pipi/cloudwego-api-gateway/kitex_gen/api"
 	"github.com/tim-pipi/cloudwego-api-gateway/kitex_gen/api/echo"
-	"github.com/cloudwego/kitex/client"
 )
 
 func main() {
+	// HTTP server
+	h := server.Default()
+
+	h.GET("/hello", func(c context.Context, ctx *app.RequestContext) {
+		// ctx.String(consts.StatusOK, "Hello, World!")
+		ctx.JSON(consts.StatusOK, utils.H{"message": "Hello, World!"})
+	})
+
+	h.Spin()
+
+
+	// RPC client
 	client, err := echo.NewClient("echo", client.WithHostPorts("0.0.0.0:8888"))
 	if err != nil {
 		log.Fatal(err)

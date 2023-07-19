367cd78075290711a7aa35183d2824d4f22b14bd
sheng <chengzhisheng2001@gmail.com>
2023-07-19 14:39:30 +0800

    Add support for generating by service name

--- pkg/cmd/gen.go
+++ pkg/cmd/gen.go
@@ -8,9 +8,13 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/tim-pipi/cloudwego-api-gateway/internal/config"
+	"github.com/tim-pipi/cloudwego-api-gateway/internal/dir"
 	"github.com/tim-pipi/cloudwego-api-gateway/internal/fileutils"
+	"github.com/tim-pipi/cloudwego-api-gateway/internal/service"
 )
 
 var _ = path.Join
@@ -20,10 +24,49 @@ var genCmd = &cobra.Command{
 	Use:   "gen",
 	Short: "Generates the template for an RPC server",
 	Long: `Generates the default template for an RPC server to run on Kitex.
-	This command will create a directory called kitex-template in the current directory,
-	and will generate the kitex RPC code from the template.`,
+This command will create a directory called kitex-template in the current directory,
+and will generate the kitex RPC code from the template.`,
+	Example: `cwgo gen -i path/to/idl/file.thrift Generate RPC server code from the specified IDL file
+cwgo gen -s HelloService Generate RPC server code for the specified service
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		idl := cmd.Flag("idl").Value.String()
+		serviceName := cmd.Flag("service").Value.String()
+
+		if (serviceName == "") && (idl == "") {
+			fmt.Println("Please specify either an IDL file or a service name.")
+			os.Exit(1)
+		}
+
+		if serviceName != "" {
+			cfg := config.ReadConfig()
+
+			if ok, _ := dir.Exists(cfg.IDLDir); !ok {
+				fmt.Println("IDL directory does not exist.\nPlease create the directory or set the env variable IDL_DIR to the correct path.")
+				return
+			}
+
+			services, err := service.GetServicesFromIDLDir(cfg.IDLDir)
+
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			found := false
+			for _, service := range services {
+				if strings.EqualFold(service.Name, serviceName) {
+					idl = service.Path
+					found = true
+					break
+				}
+			}
+
+			if !found {
+				fmt.Println("No service with that name found.")
+				os.Exit(1)
+			}
+		}
 
 		dir, err := os.Getwd()
 		if err != nil {
@@ -46,8 +89,11 @@ var genCmd = &cobra.Command{
 			kitexDir,
 			idl,
 		)
-		if out, err := kitexCmd.Output(); err != nil {
-			fmt.Println(string(out))
+
+		kitexCmd.Stdout = os.Stdout
+		kitexCmd.Stderr = os.Stderr
+
+		if err := kitexCmd.Run(); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
@@ -57,6 +103,7 @@ var genCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(genCmd)
 	// Add flag for IDL file
-	genCmd.Flags().StringP("idl", "i", "", "Thrift IDL file to use for generating the RPC server")
-	genCmd.MarkFlagRequired("idl")
+	genCmd.Flags().StringP("idl", "i", "", "Thrift IDL file to use for generating RPC server code")
+	genCmd.Flags().StringP("service", "s", "", "Service name to use for generating RPC server code")
+	genCmd.MarkFlagsMutuallyExclusive("idl", "service")
 }

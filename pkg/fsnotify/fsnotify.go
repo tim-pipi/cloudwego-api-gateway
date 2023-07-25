dacf25e43c8dcf1e51b3202885da3cc85f8436ec
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-07-25 14:03:16 +0800

    Refactor fsnotify.go file

--- pkg/fsnotify/fsnotify.go
+++ pkg/fsnotify/fsnotify.go
@@ -26,100 +26,50 @@ func newWatcher(dir string) *fsnotify.Watcher {
     return watcher
 }
 
-func main() {
-	// Create new watcher. 
-    tempIdlPath := "./examples/hello/idl_temp"
-    watcher := newWatcher(tempIdlPath)
-    defer watcher.Close()
-
-    idlPath := "./examples/hello/http-server/idl"
-    w := newWatcher(idlPath)
-    defer w.Close()
-
-    // Copy and edit IDL file at /temp_idl/
-    // rename IDL file to [idl_name]_temp.thrift
-    // Validate with thriftgo
-    // Copy over new IDL file to /http-server/idl/
-    // Use new IDL file -> if error, use old IDL file, log error
-    // -> if success, rename new IDL file, and replace old IDL file
-
-    // Start listening for events.
-    go func() {
-        for {
-            select {
-            case event, ok := <-watcher.Events:
-                if !ok {
-                    return
-                }
-				// log.Println("Event:", event)
-				// TODO
-				switch {
-					case event.Has(fsnotify.Write):
-						log.Println("Modified file:" + event.Name)
-					case event.Has(fsnotify.Create):
-						log.Println("Created new file:" + event.Name)
-                        // TODO: Use the parser to validate
-                        _, err := parser.ParseFile(event.Name, []string{""}, true)
-
-                        if err != nil {
-                            log.Println("Handle Error here")
-                        }
-                    
-                        log.Println("Valid IDL")
-
-					case event.Has(fsnotify.Remove):
-						log.Println("Removed file: " + event.Name)
-					case event.Has(fsnotify.Rename):
-						log.Println("Renamed file: " + event.Name)
-					case event.Has(fsnotify.Chmod):
-						log.Printf("Chmod: " + event.Name)
-				}
-            case err, ok := <-watcher.Errors:
-                if !ok {
-                    return
-                }
-                log.Println("error:", err)
+func runWatcher(watcher *fsnotify.Watcher) {
+    for {
+        select {
+        case event, ok := <-watcher.Events:
+            if !ok {
+                return
             }
-        }
-    }()
 
-    go func() {
-        for {
-            select {
-            case event, ok := <-w.Events:
-                if !ok {
-                    return
-                }
-				// log.Println("Event:", event)
-				// TODO
-				switch {
-					case event.Has(fsnotify.Write):
-						log.Println("Modified file:" + event.Name)
-					case event.Has(fsnotify.Create):
-						log.Println("Created new file: " + event.Name)
-                        log.Println("CHECK IF IDL WORKS FINE HERE")
-                        // TODO: Check if new IDL file works fine
-                        // _, err := check()
+            if event.Has(fsnotify.Create) {
+                log.Println("Created new file:" + event.Name)
+                _, err := parser.ParseFile(event.Name, []string{""}, true)
 
-                        // if err != nil {
-                        //     return 
-                        // }
-					case event.Has(fsnotify.Remove):
-						log.Println("Removed file: " + event.Name)
-					case event.Has(fsnotify.Rename):
-						log.Println("Renamed file: " + event.Name)
-					case event.Has(fsnotify.Chmod):
-						log.Printf("Chmod: " + event.Name)
-				}
-            case err, ok := <-w.Errors:
-                if !ok {
-                    return
+                if err != nil {
+                    log.Println("IDL file is invalid!")
+                    continue
                 }
-                log.Println("error:", err)
+            
+                log.Println("IDL file is valid!")
             }
-        }
-    }()
 
-    // Block main goroutine forever.
-    <-make(chan struct{})
+        case err, ok := <-watcher.Errors:
+            if !ok {
+                return
+            }
+            log.Println("error:", err)
+        }
+    }
 }
+
+// func main() {
+// 	// Create new watcher.
+//     idlPath := "./pkg/fsnotify/idltest"
+//     watcher := newWatcher(idlPath)
+//     defer watcher.Close()
+
+//     // Copy and edit IDL file at /temp_idl/
+//     // rename IDL file to [idl_name]_temp.thrift
+//     // Validate with thriftgo
+//     // Copy over new IDL file to /http-server/idl/
+//     // Use new IDL file -> if error, use old IDL file, log error
+//     // -> if success, rename new IDL file, and replace old IDL file
+
+//     // Start listening for events.
+//     go runWatcher(watcher)
+//     // Block main goroutine forever.
+//     <-make(chan struct{})
+// }

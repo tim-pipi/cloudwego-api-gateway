563caf0b3a41afb54f7d0643bcfe0d6ac622d60e
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-07-25 14:53:04 +0800

    Refactor fsnotify

--- pkg/fsnotify/fsnotify.go
+++ pkg/fsnotify/fsnotify.go
@@ -2,79 +2,69 @@ package fsnotify
 
 import (
     "log"
+    "os"
+    "os/exec"
 
     "github.com/fsnotify/fsnotify"
     "github.com/cloudwego/thriftgo/parser"
 )
 
-func NewWatcher(dir string) *fsnotify.Watcher {
-    watcher, err := fsnotify.NewWatcher()
-    log.Println("Running Watcher for " + dir)
+func WatchIDLFiles(idlDir string) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+                // if event.Has(fsnotify.Write) {
+				// 	log.Printf("File %s modified, triggering code generation...", event.Name)
+				// 	// Call the code generation process here
+				// 	cmd := exec.Command("go", "generate", "your/package/containing/generated/code")
+				// 	cmd.Stdout = os.Stdout
+				// 	cmd.Stderr = os.Stderr
+				// 	if err := cmd.Run(); err != nil {
+				// 		log.Println("Code generation failed:", err)
+				// 	}
+				// }
+                if event.Has(fsnotify.Create) {
+                    log.Println("File %s created, parsing IDL file...", event.Name)
+                    _, err := parser.ParseFile(event.Name, []string{""}, true)
     
-    if err != nil {
-        log.Fatal(err)
-    }
+                    if err != nil {
+                        log.Println("File %s is invalid. Please ensure your file is correct.", event.Name)
+                        continue
+                    }
+                
+                    log.Println("File %s is valid, performing hot reload...", event.Name)
+                    cmd := exec.Command("echo", "Hi there")
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
 
-    err = watcher.Add(dir)
-    log.Println("Watching files at " + dir)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-
-    return watcher
-}
-
-func RunWatcher(watcher *fsnotify.Watcher) {
-    for {
-        select {
-        case event, ok := <-watcher.Events:
-            if !ok {
-                return
-            }
-
-            if event.Has(fsnotify.Create) {
-                log.Println("Created new file:" + event.Name)
-                _, err := parser.ParseFile(event.Name, []string{""}, true)
-
-                if err != nil {
-                    log.Println("IDL file is invalid!")
-                    continue
+                    if err := cmd.Run(); err != nil {
+						log.Println("Code generation failed:", err)
+					}
                 }
-            
-                log.Println("IDL file is valid!")
-            }
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				log.Println("Error:", err)
+			}
+		}
+	}()
 
-        case err, ok := <-watcher.Errors:
-            if !ok {
-                return
-            }
-            log.Println("error:", err)
-        }
-    }
+	err = watcher.Add(idlDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
 }
-
-// Example on how to run:
-
-/* 
-func main() {
-	// Create new watcher.
-    idlPath := "./pkg/fsnotify/idltest"
-    watcher := newWatcher(idlPath)
-    defer watcher.Close()
-
-    // Copy and edit IDL file at /temp_idl/
-    // rename IDL file to [idl_name]_temp.thrift
-    // Validate with thriftgo
-    // Copy over new IDL file to /http-server/idl/
-    // Use new IDL file -> if error, use old IDL file, log error
-    // -> if success, rename new IDL file, and replace old IDL file
-
-    // Start listening for events.
-    go runWatcher(watcher)
-    // Block main goroutine forever.
-    <-make(chan struct{})
-}
-
-*/
